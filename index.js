72c3b65e7448c7b64d654f18c53e9e6bd4eaedac
Nilambar Sharma <nilambar@outlook.com>
2022-08-13 17:14:26 +0545

    fix: function file

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 #!/usr/bin/env node
 
-console.log( `Hello world` );
+import meow from 'meow';
+
+const foo = (input, flags) => {
+  console.log( input, 'input' );
+  console.log( flags, 'flags' );
+}
+
+const cli = meow(`
+  Usage
+    $ foo <input>
+
+  Options
+    --rainbow, -r  Include a rainbow
+
+  Examples
+    $ foo unicorns --rainbow
+    🌈 unicorns 🌈
+`, {
+  importMeta: import.meta,
+  flags: {
+    rainbow: {
+      type: 'boolean',
+      alias: 'r'
+    }
+  }
+});
+
+foo(cli.input[0], cli.flags);
