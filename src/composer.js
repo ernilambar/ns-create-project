4f8725bed4e32c2eaac12070d1f58bc69feebe75
Nilambar Sharma <nilambar@outlook.com>
2022-10-03 19:49:17 +0545

    fix: composer setup

--- src/composer.js
+++ src/composer.js
@@ -7,10 +7,10 @@ const ncpUpdateComposerJsonContent = (jsonContent) => {
     scripts: {
       'pc:info': '@php ./vendor/squizlabs/php_codesniffer/bin/phpcs -i',
       'pc:config': '@php ./vendor/squizlabs/php_codesniffer/bin/phpcs --config-show',
-      compat: '@php ./vendor/squizlabs/php_codesniffer/bin/phpcs --standard=.phpcompat.xml.dist --report-full --report-summary .',
       lint: '@php ./vendor/squizlabs/php_codesniffer/bin/phpcs --report-full --report-summary .',
       'lint:error': '@lint -n',
-      'lint:fix': '@php ./vendor/squizlabs/php_codesniffer/bin/phpcbf --report-full --report-summary .'
+      'lint:fix': '@php ./vendor/squizlabs/php_codesniffer/bin/phpcbf --report-full --report-summary .',
+      compat: '@php ./vendor/squizlabs/php_codesniffer/bin/phpcs --standard=.phpcompat.xml.dist --report-full --report-summary .'
     }
   })
 
